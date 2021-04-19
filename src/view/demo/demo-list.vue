23fb7253f5b82f5c1656ae6aa328dff9f834ec33
yang <819836692@qq.com>
2021-04-19 15:41:27 +0800

    demo

--- src/view/demo/demo-list.vue
+++ src/view/demo/demo-list.vue
@@ -1,13 +1,79 @@
 <template>
   <div>
-    demo
+    <!-- 列表页面 -->
+    <div class="container">
+      <div class="header"><div class="title">demo列表</div></div>
+      <!-- 表格 -->
+      <lin-table
+        :tableColumn="tableColumn"
+        :tableData="tableData"
+        :operate="operate"
+        @handleEdit="handleEdit"
+        @handleDelete="handleDelete"
+        @row-click="rowClick"
+        v-loading="loading"
+      ></lin-table>
+    </div>
   </div>
 </template>
 
 <script>
+import LinTable from '@/component/base/table/lin-table'
+import { JeecgListMixin } from '@/mixins/JeecgListMixin'
+
 export default {
-  name: 'demo',
+  components: {
+    LinTable,
+  },
+  mixins: [JeecgListMixin],
+  data() {
+    return {
+      tableColumn: [{ prop: 'title', label: '书名' }, { prop: 'author', label: '作者' }],
+      tableData: [],
+      operate: [],
+      showEdit: false,
+      editBookID: 1,
+    }
+  },
+  async created() {
+    this.loading = true
+    await this.getBooks()
+    this.operate = [
+      { name: '编辑', func: 'handleEdit', type: 'primary' },
+      {
+        name: '删除',
+        func: 'handleDelete',
+        type: 'danger',
+        permission: '删除图书',
+      },
+    ]
+    this.loading = false
+  },
 }
 </script>
 
-<style scoped></style>
+<style lang="scss" scoped>
+.container {
+  padding: 0 30px;
+
+  .header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    .title {
+      height: 59px;
+      line-height: 59px;
+      color: $parent-title-color;
+      font-size: 16px;
+      font-weight: 500;
+    }
+  }
+
+  .pagination {
+    display: flex;
+    justify-content: flex-end;
+    margin: 20px;
+  }
+}
+</style>
